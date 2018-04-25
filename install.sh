aae25023321a13cb22ccb1e232d23a364bd73f53
Fumiaki Kato <kato_f@worksap.co.jp>
2018-04-25 20:14:34 +0900

    parameterized some path and version

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+osclass_version=3.7.4
+domain_name=example.com
+osclass_root="/var/www/${domain_name}/public_html"
+
 echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
 
@@ -23,16 +27,16 @@ sudo apt-get install -y nginx \
 sudo mysql -u root -proot -e "CREATE DATABASE osclass; GRANT ALL PRIVILEGES ON osclass.* TO 'osclass' IDENTIFIED BY 'osclass';"
 
 # nginx settings
-sudo mkdir -p /var/www/example.com/public_html
+sudo mkdir -p ${osclass_root}
 
-sudo tee /etc/nginx/sites-available/example.com << EOF
+sudo tee /etc/nginx/sites-available/${domain_name} << EOF
 server {
     listen 80;
     listen [::]:80;
 
-    server_name example.com;
+    server_name ${domain_name};
 
-    root   /var/www/example.com/public_html;
+    root   ${osclass_root};
     index  index.html index.php;
 
     location / {
@@ -43,23 +47,22 @@ server {
         include snippets/fastcgi-php.conf;
         include fastcgi_params;
         fastcgi_pass unix:/run/php/php7.0-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME /var/www/example.com/public_html\$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME ${osclass_root}\$fastcgi_script_name;
     }
 }
 EOF
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/${domain_name} /etc/nginx/sites-enabled
 
 # for osclass
-wget https://static.osclass.org/download/osclass.3.7.0.zip -P ~
-sudo unzip ~/osclass.3.7.0.zip -d /var/www/example.com/public_html
-sudo chown -R www-data:www-data /var/www/example.com/
-sudo chown -R www-data:www-data /var/www/example.com/public_html
-sudo chmod a+w /var/www/example.com/public_html/oc-content/uploads/
-sudo chmod a+w /var/www/example.com/public_html/oc-content/downloads/
-sudo chmod a+w /var/www/example.com/public_html/oc-content/languages/
-sudo chmod a+w /var/www/example.com/public_html/
+wget https://static.osclass.org/download/osclass.${osclass_version}.zip -P ~
+sudo unzip ~/osclass.${osclass_version}.zip -d ${osclass_root}
+sudo chown -R www-data:www-data ${osclass_root}
+sudo chmod a+w ${osclass_root}/oc-content/uploads/ \
+${osclass_root}/oc-content/downloads/ \
+${osclass_root}/oc-content/languages/ \
+${osclass_root}
 
 # restart nginx and php
 sudo systemctl restart php7.0-fpm nginx
